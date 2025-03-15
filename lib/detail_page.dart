a6ed2ff331fc4b01709a57dcb332b84cd9aef13e
suhyunChoo <s_where@naver.com>
2025-03-15 12:43:03 +0900

    feat: 홈페이지 데이터 바인딩

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -1,10 +1,61 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatelessWidget {
-  const DetailPage({super.key});
+  final List csvRow;
+  const DetailPage({super.key, required this.csvRow});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('상품 상세'),
+      ),
+      body: Expanded(
+          child: Column(
+        children: [
+          Container(
+            height: 300,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/${csvRow[1]}.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      CupertinoIcons.person_circle_fill,
+                      color: Colors.orange,
+                      size: 60,
+                    ),
+                    Column(
+                      children: [
+                        Text('data'),
+                        Text('data'),
+                      ],
+                    ),Column(
+                      children: [
+                        Text('39.3 °C'),
+                        Text('매너온도'),
+                      ],
+                    ),
+
+                  ],
+                ),
+                Text('data'),
+                Text('data'),
+              ],
+              
+            ),
+          )
+        ],
+      )),
+    );
   }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,38 @@
+import 'package:csv/csv.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_applemarket_app/detail_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<List<dynamic>> csvData = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCSV();
+  }
+
+  Future<void> _loadCSV() async {
+    try {
+      final rawData = await rootBundle.loadString('assets/data/dummydata.csv');
+      List<List<dynamic>> listData =
+          const CsvToListConverter().convert(rawData);
+
+      setState(() {
+        csvData = listData.sublist(2);
+      });
+    } catch (e) {
+      print("Error loading CSV: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,42 +43,84 @@ class HomePage extends StatelessWidget {
             IconButton(
               icon: Icon(CupertinoIcons.bell),
               onPressed: () {
-                // 아이콘 클릭 시 동작
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('이 메시지는 SnackBar로 표시됩니다!'),
+                    duration: Duration(seconds: 2),
+                  ),
+                );
               },
             ),
           ],
           backgroundColor: Colors.grey[200],
         ),
         body: ListView.builder(
-            itemCount: 10,
+            itemCount: csvData.length,
             itemBuilder: (context, index) {
+              final csvRow = csvData[index];
               return Padding(
                 padding:
                     const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                child: Container(
-                  height: 120,
-                  decoration: BoxDecoration(color: Colors.grey[100]),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => DetailPage(csvRow: csvRow,)));
+                  },
+                  child: Container(
+                    height: 140,
+                    decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(8)),
                     child: Row(
                       children: [
-                        Container(child: Text('image')),
                         Container(
+                          width: 100,
+                          height: double.infinity,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/${csvRow[1]}.png'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(10),
                             child: Column(
-                          children: [
-                            Text('김치냉장고'),
-                            Text('인천 서구 가정로'),
-                            Text('3,000원'),
-                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Icon(CupertinoIcons.chat_bubble_2),
-                                Text('3'),
-                                Icon(CupertinoIcons.heart),
-                                Text('3'),
+                                Text(
+                                  csvRow[2],
+                                  style: TextStyle(
+                                      fontSize: 16, fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  csvRow[6],
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.grey,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  ' ${csvRow[5].toString()}원',
+                                  style: TextStyle(
+                                      fontSize: 16, fontWeight: FontWeight.bold),
+                                ),
+                                Spacer(),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Icon(CupertinoIcons.chat_bubble_2),
+                                    Text(csvRow[8].toString()),
+                                    Icon(CupertinoIcons.heart),
+                                    Text(csvRow[7].toString()),
+                                  ],
+                                )
                               ],
-                            )
-                          ],
-                        )),
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                   ),
